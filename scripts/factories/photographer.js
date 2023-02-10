99f912102d99b5b2cda1f6f4094e3457c96d465c
Sayaka Vincent <sfv2010@hotmail.fr>
2023-02-10 13:51:05 +0100

    overall fix

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,51 @@
-function photographerFactory(data) {
-    const { name, portrait } = data;
+//Function that creates a photographer card
+function photographerFactory(photographer) {
+    //Object destructuring assignment. Extract multiple properties from object
+    const { city, country, id, name, portrait, price, tagline } = photographer;
+    //const city = photograher.city..
 
     const picture = `assets/photographers/${portrait}`;
-
+    //Function that returns the photographer DOM element
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+        // Create article element
+        const articlePhotographer = document.createElement("article");
+        // Create link
+        const linkPhotographer = document.createElement("a");
+        linkPhotographer.href = `photographer.html?id= ${id} `;
+        linkPhotographer.setAttribute(
+            "aria-label",
+            `Lien vers le portfolio de ${name}`
+        );
+        // Create image
+        const imgPhotographer = document.createElement("img");
+        imgPhotographer.src = picture;
+        imgPhotographer.alt = `Portrait de ${name}`;
+        // Create h2
+        const h2Photographer = document.createElement("h2");
+        h2Photographer.textContent = name;
+        // Create div
+        const divPhotographer = document.createElement("div");
+        divPhotographer.classList.add("introduction");
+        divPhotographer.tabIndex = "0";
+
+        // Create h3
+        const Photographer = document.createElement("h3");
+        Photographer.textContent = city + ", " + country;
+        // Create p
+        const pPhotographer = document.createElement("p");
+        pPhotographer.textContent = tagline;
+        // Create span
+        const spanPhotographer = document.createElement("span");
+        spanPhotographer.innerText = `${price} €/jour`;
+
+        articlePhotographer.appendChild(linkPhotographer);
+        linkPhotographer.appendChild(imgPhotographer);
+        linkPhotographer.appendChild(h2Photographer);
+        articlePhotographer.appendChild(divPhotographer);
+        divPhotographer.appendChild(Photographer);
+        divPhotographer.appendChild(pPhotographer);
+        divPhotographer.appendChild(spanPhotographer);
+        return articlePhotographer;
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, getUserCardDOM };
 }
