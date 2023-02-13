382023312518a860d4bf1efe00ee09b00e2b7f76
Sayaka Vincent <sfv2010@hotmail.fr>
2023-02-13 12:42:42 +0100

    Add comment

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -6,20 +6,25 @@ async function displayData(photographers) {
         const photographerInfo = photographerIntroduction(photographer);
         console.log(photographerInfo);
         //create each photographer in a Dom element
-        const userCardDOM = photographerInfo.getPhotgrapherCardDOM();
+        const userCardDOM = photographerInfo.getPhotographerCardDOM();
         photographersSection.appendChild(userCardDOM);
     });
 }
 //Function to get,selection and display photographer data
 async function init() {
+    //media or photographers
     const { photographers } = await getPhotographers();
-    console.log("photographers=" + photographers[0]);
-    const idPhotographer = window.location.href.split("?")[1];
+    //console.log("photographers=" + photographers[0]);
+    //split("?")[1] = id, split("?")[0]= http...
+    const idPhotographer = location.href.split("?")[1];
     console.log(idPhotographer);
-    const photographer = await photographers.filter(
+    //typeof idPhotographer = Sting
+    const SelectedPhotographer = await photographers.filter(
         (photographer) => photographer.id == idPhotographer
     );
-    console.log("photographer=" + photographer);
-    displayData(photographer);
+
+    //pourquoi sur console on ne peut pas
+    console.log("photographer=" + SelectedPhotographer);
+    displayData(SelectedPhotographer);
 }
 init();
