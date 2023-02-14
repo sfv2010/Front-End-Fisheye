23acc41251637e038b329b208934a123312fedeb
Sayaka Vincent <sfv2010@hotmail.fr>
2023-02-14 09:48:25 +0100

    Add to display the number of likes

--- scripts/factories/photographerIntroduction.js
+++ scripts/factories/photographerIntroduction.js
@@ -1,8 +1,7 @@
 //Factory Function that creates a photographer introduction
 function photographerIntroduction(photographer) {
     //Object destructuring assignment. Extract multiple properties from object
-    const { city, country, name, portrait, tagline } = photographer;
-    //const city = photographer.city..
+    const { city, country, name, portrait, tagline, price } = photographer;
 
     const picture = `assets/photographers/${portrait}`;
     //Function that returns the photographer DOM element
@@ -22,7 +21,6 @@ function photographerIntroduction(photographer) {
         // Create h2
         const h2Photographer = document.createElement("h2");
         h2Photographer.textContent = name;
-
         // Create h3
         const h3Photographer = document.createElement("h3");
         h3Photographer.textContent = city + ", " + country;
@@ -35,12 +33,30 @@ function photographerIntroduction(photographer) {
         // buttonPhotographer.onclick = "displayModal()";
         buttonPhotographer.textContent = "Contactez-moi";
 
+        //create div pour small display
+        const divSmallDisplay = document.createElement("div");
+        divSmallDisplay.classList.add("likePrice");
+        divSmallDisplay.tabIndex = "0";
+        const pLike = document.createElement("p");
+        pLike.textContent = "nombre de like";
+        const iconLike = document.createElement("i");
+        iconLike.classList.add("fa-solid");
+        iconLike.classList.add("fa-heart");
+        const pPrice = document.createElement("p");
+        pPrice.textContent = `${price} €/jour`;
+
         articlePhotographer.appendChild(divPhotographer);
         articlePhotographer.appendChild(buttonPhotographer);
         articlePhotographer.appendChild(imgPhotographer);
+        articlePhotographer.appendChild(divSmallDisplay);
+
         divPhotographer.appendChild(h2Photographer);
         divPhotographer.appendChild(h3Photographer);
         divPhotographer.appendChild(pPhotographer);
+
+        divSmallDisplay.appendChild(pLike);
+        divSmallDisplay.appendChild(pPrice);
+        pLike.appendChild(iconLike);
 
         return articlePhotographer;
     }
