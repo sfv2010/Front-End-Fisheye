21e0276d8ce812e21fd9dc2e8eb1a477b190795f
Sayaka Vincent <sfv2010@hotmail.fr>
2023-02-15 23:07:41 +0100

    add Event

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,13 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+    modal.style.display = "block";
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
+window.onload = function () {
+    const contact = document.querySelector(".contact_photographer");
+    contact.addEventListener("click", displayModal);
+};
