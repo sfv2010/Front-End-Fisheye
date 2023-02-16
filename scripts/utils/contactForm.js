c75be66123feefa6db4e4cab6d2aab63388e1d9b
Sayaka Vincent <sfv2010@hotmail.fr>
2023-02-16 14:15:04 +0100

    Add ariaHidden

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,6 +1,11 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
+    const main = document.getElementById("main");
+    // const closeButton = document.querySelector(".closeModal");
     modal.style.display = "block";
+    main.ariaHidden = "true";
+    modal.ariaHidden = "false";
+    // closeButton.focus();
 }
 
 function closeModal() {
@@ -9,5 +14,6 @@ function closeModal() {
 }
 window.onload = function () {
     const contact = document.querySelector(".contact_photographer");
+    console.log(contact);
     contact.addEventListener("click", displayModal);
 };
