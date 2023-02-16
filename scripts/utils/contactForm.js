d1773ad08b83a501d0793d97be55814fde80aed6
Sayaka Vincent <sfv2010@hotmail.fr>
2023-02-16 17:35:45 +0100

    Fix displayModal

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,18 +1,23 @@
-function displayModal() {
-    const modal = document.getElementById("contact_modal");
-    const main = document.getElementById("main");
-    // const closeButton = document.querySelector(".closeModal");
-    modal.style.display = "block";
-    main.ariaHidden = "true";
-    modal.ariaHidden = "false";
-    // closeButton.focus();
-}
-
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
+// document.addEventListener("load", function () {
+//     const contact = document.querySelector(".contact_photographer");
+//     console.log(contact);
+//     contact.addEventListener("click", displayModal);
+// });
+
 window.onload = function () {
+    function displayModal() {
+        const modal = document.getElementById("contact_modal");
+        const main = document.getElementById("main");
+        // const closeButton = document.querySelector(".closeModal");
+        modal.style.display = "block";
+        main.ariaHidden = "true";
+        modal.ariaHidden = "false";
+        // closeButton.focus();
+    }
     const contact = document.querySelector(".contact_photographer");
     console.log(contact);
     contact.addEventListener("click", displayModal);
