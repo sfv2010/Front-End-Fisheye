c1a77c3697fd7829c8d044f7ed6b7108009a9697
Sayaka Vincent <sfv2010@hotmail.fr>
2023-02-17 02:06:51 +0100

    Add focus

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,24 +1,23 @@
+function displayModal() {
+    const modal = document.getElementById("contact_modal");
+    const main = document.getElementById("main");
+    const focusInput = document.getElementById("lastName");
+    modal.style.display = "block";
+    main.ariaHidden = "true";
+    modal.ariaHidden = "false";
+    focusInput.focus();
+}
 function closeModal() {
     const modal = document.getElementById("contact_modal");
+    const main = document.getElementById("main");
     modal.style.display = "none";
-}
-// document.addEventListener("load", function () {
-//     const contact = document.querySelector(".contact_photographer");
-//     console.log(contact);
-//     contact.addEventListener("click", displayModal);
-// });
 
-window.onload = function () {
-    function displayModal() {
-        const modal = document.getElementById("contact_modal");
-        const main = document.getElementById("main");
-        // const closeButton = document.querySelector(".closeModal");
-        modal.style.display = "block";
-        main.ariaHidden = "true";
-        modal.ariaHidden = "false";
-        // closeButton.focus();
-    }
-    const contact = document.querySelector(".contact_photographer");
-    console.log(contact);
-    contact.addEventListener("click", displayModal);
-};
+    main.ariaHidden = "false";
+    modal.ariaHidden = "true";
+}
+const contact = document.querySelector(".contact_photographer");
+console.log(contact);
+contact.addEventListener("click", displayModal);
+
+const closeModalButton = document.querySelector(".closeModal");
+closeModalButton.addEventListener("click", closeModal);
