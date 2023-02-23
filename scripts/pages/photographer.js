90441c4d07ed16d68a80538ef033f63465f94875
Sayaka Vincent <sfv2010@hotmail.fr>
2023-02-23 10:49:17 +0100

    Add viewLightbox and LightboxFactory

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -2,7 +2,10 @@ import { getPhotographers } from "../utils/service.js";
 import { photographerIntroduction } from "../factories/photographerIntroduction.js";
 import { mediaFactory } from "../factories/mediaFactory.js";
 import {} from "../utils/contactForm.js";
+import { viewLightbox } from "../utils/lighthbox.js";
 import { timesLiked } from "../utils/likes.js";
+// import { orderBy } from "../utils/sort.js";
+import { lightboxFactory } from "../factories/lightboxFactory.js";
 
 // ---------------------------photographer--------------------------
 //Function to create selected photographer
@@ -38,26 +41,84 @@ init();
 //Function to create photos of selected photographer
 async function displayMedia(medias) {
     const mediaSection = document.querySelector(".containerMediaWork");
-    //Loop through the Photographers array and create a profile for each
+    //Loop through the media array and create photo
     medias.forEach((media) => {
         const mediaInfo = mediaFactory(media);
-        //create each photographer in a Dom element
+        //create Dom element
         const userCardDOM = mediaInfo.getMediaCardDOM();
         mediaSection.appendChild(userCardDOM);
     });
+    // const lightboxSection = document.getElementById("lightboxModal");
+    // const lightboxInfo = lightboxFactory(medias);
+    // const lightboxCardDOM = lightboxInfo.getLightboxCardDOM();
+    // lightboxSection.appendChild(lightboxCardDOM);
+
+    //Execute the timesliked function
     timesLiked();
+    //Execute the viewLightbox function
+    // viewLightbox();
+}
+async function displayLightbox(medias) {
+    const lightboxSection = document.getElementById("lightboxModal");
+
+    const lightboxInfo = lightboxFactory(medias);
+    const lightboxCardDOM = lightboxInfo.getLightboxCardDOM();
+    lightboxSection.appendChild(lightboxCardDOM);
+    viewLightbox();
 }
 //Function to get,selection and display photo data
-async function initMedia() {
+export async function initMedia() {
     //media or photographers
     const { media } = await getPhotographers();
-    console.log("media=" + media[0]);
     //split("?")[1] = id, split("?")[0]= http...
     const idPhotographer = location.href.split("?")[1];
     //typeof idPhotographer = Sting
-    const SelectedPhoto = await media.filter(
+    const selectedPhoto = await media.filter(
         (media) => media.photographerId == idPhotographer
     );
-    displayMedia(SelectedPhoto);
+    // const lightboxPhoto = await selectedPhoto.find(
+    //              (media) => media.id == idLightbox.value
+    //          );
+    const copySelectedPhoto = [...selectedPhoto];
+    copySelectedPhoto.sort((a, b) => b.likes - a.likes);
+
+    const select = document.getElementById("dropDownMenu");
+    select.addEventListener("change", () => {
+        if (select.value === "Date") {
+            console.log(select.value);
+            copySelectedPhoto.sort(
+                (a, b) => new Date(b.date) - new Date(a.date)
+            );
+        } else if (select.value === "popularity") {
+            copySelectedPhoto.sort((a, b) => b.likes - a.likes);
+        } else if (select.value === "Titre") {
+            console.log(select.value);
+            copySelectedPhoto.sort((a, b) =>
+                a.title.toLowerCase() < b.title.toLowerCase() ? -1 : 1
+            );
+        }
+    });
+
+    displayMedia(copySelectedPhoto);
+
+    displayLightbox(selectedPhoto);
 }
 initMedia();
+
+// async function lightboxMedia() {
+//     const lightboxSelected = document.querySelectorAll(".viewIn");
+//     console.log(lightboxSelected);
+//     const { media } = await getPhotographers();
+//     const idPhotographer = location.href.split("?")[1];
+//     // typeof idPhotographer = Sting
+//     const selectedPhoto = await media.filter(
+//         (media) => media.photographerId == idPhotographer
+//     );
+//     // await lightboxSelected.forEach((elem) => {
+//     //     console.log(lightboxSelected);
+//     //     elem.addEventListener("click", displayLightbox(selectedPhoto));
+//     // });
+
+//     displayLightbox(selectedPhoto);
+// }
+// lightboxMedia();
