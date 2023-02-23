237740665add10a55e4aeba11510289f242c0652
Sayaka Vincent <sfv2010@hotmail.fr>
2023-02-23 15:09:02 +0100

    removed console.log

--- scripts/utils/likes.js
+++ scripts/utils/likes.js
@@ -2,16 +2,13 @@ export function timesLiked() {
     const likeIt = document.querySelectorAll(".divLike");
     const totalLikes = document.querySelector(".totalLikes");
     const numberOfLike = document.querySelectorAll(".numberOfLike");
-    console.log(numberOfLike);
 
     //Display the total of likes in small footer
     let totalNumberLikes = 0;
     numberOfLike.forEach((like) => {
         totalNumberLikes += Number(like.textContent);
-        console.log(totalNumberLikes);
         totalLikes.textContent = totalNumberLikes;
     });
-    // && && liked.classList.contain("clicked")
     likeIt.forEach((liked) => {
         window.addEventListener("load", () => {});
 
