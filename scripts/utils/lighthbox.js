e9dc988acea2aad62a43539bf4ef75215fa75813
Sayaka Vincent <sfv2010@hotmail.fr>
2023-03-01 14:00:22 +0100

    add mediaNext

--- scripts/utils/lighthbox.js
+++ scripts/utils/lighthbox.js
@@ -5,72 +5,79 @@ export function viewLightbox() {
     const allImages = document.querySelectorAll(".viewIn");
     const main = document.getElementById("main");
     const closeLightboxModalButton = document.querySelector(".closeLightbox");
+    const mediaNext = document.querySelector(".chevronNext");
+    const mediaBack = document.querySelector(".chevronBack");
+    let currentId;
+
+    const arrayAllImages = Array.from(allImages);
     const figure = document.querySelector("figure");
+    const zoomLightbox = document.querySelector(".viewInLightbox");
+    const figcaption = document.querySelector("figcaption");
 
     function displayLightboxModal(e) {
-        const zoomLightbox = document.querySelector(".viewInLightbox");
-
-        const figcaption = document.querySelector("figcaption");
-        const arrayAllImages = Array.from(allImages);
-        console.log(arrayAllImages);
+        // console.log(arrayAllImages);
 
         lightboxModal.style.display = "block";
         main.ariaHidden = "true";
         lightboxModal.ariaHidden = "false";
 
         const media = e.target;
-        //  const media = e.target.cloneNode(true);
-        // media.classList.remove("viewIn");
-        // media.classList.add("viewInLightbox");
-        // figure.insertBefore(media, figcaption);
-        // console.log(figcaption);
-        // // figure.removeChild(zoomLightbox);
 
-        // figcaption.innerText = e.target.getAttribute("title");
-
-        const currentId = media.id;
+        currentId = media.id;
         lightboxMedia(currentId);
 
         // console.log(currentId);
 
-        // const currentIndex = arrayAllImages.findIndex(
-        //     (current) => current.id === currentId
-        // );
-        // function showNext() {
-        //     if (currentIndex < arrayAllImages.length - 1) {
-        //         const nextIndex =
-        //             arrayAllImages[currentIndex + 1].cloneNode(true);
-        //         console.log(nextIndex);
-        //         // media.id = nextIndex;
-        //         nextIndex.classList.remove("viewIn");
-        //         nextIndex.classList.add("viewInLightbox");
-        //         figure.insertBefore(nextIndex, figcaption);
-        //         figure.removeChild(media);
-        //     } else {
-        //         const nextIndexMedia = arrayAllImages[0].cloneNode(true);
-        //         nextIndexMedia.classList.remove("viewIn");
-        //         nextIndexMedia.classList.add("viewInLightbox");
-        //         figure.insertBefore(nextIndexMedia, figcaption);
-        //         figure.removeChild(media);
-        //     }
-        // }
-
-        // console.log(currentIndex);
-
-        // const mediaNext = document.querySelector(".chevronNext");
-        // const mediaBack = document.querySelector(".chevronBack");
-
-        // mediaNext.addEventListener("click", showNext);
-
-        // mediaBack.addEventListener("click", showBack);
-        // function showBack() {}
-
         //zoomLightbox.setAttribute("src", e.target.getAttribute("id"));
     }
+
+    function showNext(e) {
+        // const arrayAllImages = Array.from(allImages);
+        const zoomLightbox = document.querySelector(".viewInLightbox");
+        const figcaption = document.querySelector("figcaption");
+
+        let currentIndex = arrayAllImages.findIndex(
+            (current) => current.id === currentId
+        );
+        // e.preventDefault;
+
+        // console.log(zoomLightbox);
+        // for (let i=0; i<=arrayAllImages.length;i++)
+        if (currentIndex < arrayAllImages.length - 1) {
+            const nextIndex = arrayAllImages[currentIndex + 1].id;
+            currentIndex++;
+            console.log(currentIndex);
+            lightboxMedia(nextIndex);
+            zoomLightbox.remove();
+            figcaption.remove();
+        } else {
+            const nextIndex = arrayAllImages[0].id;
+            currentIndex = 0;
+            lightboxMedia(nextIndex);
+            console.log(currentIndex);
+            zoomLightbox.remove();
+            figcaption.remove();
+        }
+
+        // zoomLightbox.remove();
+        // figcaption.remove();
+    }
+
+    // console.log(currentIndex);
+
+    mediaNext.addEventListener("click", showNext);
+
+    mediaBack.addEventListener("click", showBack);
+    function showBack() {}
     function closeLightboxModal() {
+        const zoomLightbox = document.querySelector(".viewInLightbox");
+        const figcaption = document.querySelector("figcaption");
+
         lightboxModal.style.display = "none";
         main.ariaHidden = "false";
         lightboxModal.ariaHidden = "true";
+        zoomLightbox.remove();
+        figcaption.remove();
     }
 
     allImages.forEach((elem) => {
@@ -80,76 +87,83 @@ export function viewLightbox() {
     closeLightboxModalButton.addEventListener("click", closeLightboxModal);
 }
 
-// import { lightboxFactory } from "../factories/lightboxFactory.js";
+// import { lightboxMedia } from "../factories/lightboxFactory.js";
 
 // export function viewLightbox() {
 //     const lightboxModal = document.getElementById("lightboxModal");
 //     const allImages = document.querySelectorAll(".viewIn");
 //     const main = document.getElementById("main");
 //     const closeLightboxModalButton = document.querySelector(".closeLightbox");
+//     const figure = document.querySelector("figure");
+//     const zoomLightbox = document.querySelector(".viewInLightbox");
+//     const figcaption = document.querySelector("figcaption");
+//     const mediaNext = document.querySelector(".chevronNext");
+//     const mediaBack = document.querySelector(".chevronBack");
+//     let currentId = 0;
+//     const arrayAllImages = Array.from(allImages);
 
 //     function displayLightboxModal(e) {
-//         const zoomLightbox = document.querySelector(".viewInLightbox");
-//         const figure = document.querySelector("figure");
-//         const figcaption = document.querySelector("figcaption");
-//         const arrayAllImages = Array.from(allImages);
-//         console.log(arrayAllImages);
+//         // console.log(arrayAllImages);
 
 //         lightboxModal.style.display = "block";
 //         main.ariaHidden = "true";
 //         lightboxModal.ariaHidden = "false";
 
-//         // const media = e.target.cloneNode(true);
-//         // media.classList.remove("viewIn");
-//         // media.classList.add("viewInLightbox");
-//         // figure.insertBefore(media, zoomLightbox);
-//         // figure.removeChild(zoomLightbox);
-//         // figcaption.innerText = e.target.getAttribute("title");
+//         const media = e.target;
 
-//         // const currentId = media.id;
+//         currentId = media.id;
+//         lightboxMedia(currentId);
+
 //         // console.log(currentId);
 
-//         // const currentIndex = arrayAllImages.findIndex(
-//         //     (current) => current.id === currentId
-//         // );
-//         // function showNext() {
-//         //     if (currentIndex < arrayAllImages.length - 1) {
-//         //         const nextIndex =
-//         //             arrayAllImages[currentIndex + 1].cloneNode(true);
-//         //         console.log(nextIndex);
-//         //         // media.id = nextIndex;
-//         //         nextIndex.classList.remove("viewIn");
-//         //         nextIndex.classList.add("viewInLightbox");
-//         //         figure.insertBefore(nextIndex, media);
-//         //         figure.removeChild(media);
-//         //     }
-//         // }
+//         function showNext(e) {
+//             const currentIndex = arrayAllImages.findIndex(
+//                 (current) => current.id === currentId
+//             );
+//             e.preventDefault;
+//             let count = currentIndex;
+//             const zoomLightbox = document.querySelector(".viewInLightbox");
+//             const figcaption = document.querySelector("figcaption");
+//             // console.log(zoomLightbox);
+
+//             if (count < arrayAllImages.length - 1) {
+//                 const nextIndex = arrayAllImages[count + 1].id;
+//                 count++;
+//                 console.log(count);
+//                 lightboxMedia(nextIndex);
+//                 zoomLightbox.remove();
+//                 figcaption.remove();
+//             } else {
+//                 const nextIndex = arrayAllImages[0].id;
+//                 count = 0;
+//                 lightboxMedia(nextIndex);
+//                 console.log(count);
+//                 zoomLightbox.remove();
+//                 figcaption.remove();
+//             }
+
+//             // zoomLightbox.remove();
+//             // figcaption.remove();
+//         }
 
 //         // console.log(currentIndex);
 
-//         // const mediaNext = document.querySelector(".chevronNext");
-//         // const mediaBack = document.querySelector(".chevronBack");
+//         mediaNext.addEventListener("click", showNext);
 
-//         // mediaNext.addEventListener("click", showNext);
+//         mediaBack.addEventListener("click", showBack);
+//         function showBack() {}
 
-//         // mediaBack.addEventListener("click", showBack);
-//         // function showBack() {}
-//         const media = e.target.cloneNode(true);
-//         console.log(media);
-//         const targetId = media.id;
-//         console.log(targetId);
-
-//         const targetLightbox = arrayAllImages.find(
-//             (target) => target.id == targetId
-//         );
-//         console.log(targetLightbox);
-//         lightboxFactory(targetLightbox);
-//         // zoomLightbox.setAttribute("src", e.target.getAttribute("src"));
+//         //zoomLightbox.setAttribute("src", e.target.getAttribute("id"));
 //     }
 //     function closeLightboxModal() {
+//         const zoomLightbox = document.querySelector(".viewInLightbox");
+//         const figcaption = document.querySelector("figcaption");
+
 //         lightboxModal.style.display = "none";
 //         main.ariaHidden = "false";
 //         lightboxModal.ariaHidden = "true";
+//         zoomLightbox.remove();
+//         figcaption.remove();
 //     }
 
 //     allImages.forEach((elem) => {
