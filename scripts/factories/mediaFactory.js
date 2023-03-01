f06388116a03c2754aad9495c82fc6e17bf38513
Sayaka Vincent <sfv2010@hotmail.fr>
2023-03-01 14:01:43 +0100

    fix divLike

--- scripts/factories/mediaFactory.js
+++ scripts/factories/mediaFactory.js
@@ -1,27 +1,26 @@
 //Factory Function that creates a photographer introduction
 export function mediaFactory(data) {
     //Object destructuring assignment. Extract multiple properties from object
-    const { id, photographerId, title, image, video, likes, date, price } =
-        data;
+    const { id, photographerId, title, image, video, likes } = data;
 
     //Function that returns the photographer DOM element
     function getMediaCardDOM() {
         // Create div
         const divMedia = document.createElement("div");
         divMedia.classList.add("divMedia");
-        divMedia.tabIndex = "0";
 
         // Create div
         const divTitleLike = document.createElement("div");
         divTitleLike.classList.add("titleLike");
-        divTitleLike.tabIndex = "0";
+
         // Create h3 for title
         const h3Media = document.createElement("h3");
         h3Media.classList.add("h3Media");
         h3Media.textContent = title;
+        h3Media.tabIndex = "0";
 
         // Create div
-        const divLike = document.createElement("div");
+        const divLike = document.createElement("button");
         divLike.classList.add("divLike");
         divLike.tabIndex = "0";
         // Create span
@@ -47,19 +46,22 @@ export function mediaFactory(data) {
             // Create video
             const videoMedia = document.createElement("video");
             videoMedia.src = mediaUrl;
-            videoMedia.alt = title;
+            videoMedia.title = title;
             videoMedia.controls = "cotrols";
             videoMedia.textContent = "Sorry, can't play video";
             videoMedia.classList.add("viewIn");
             videoMedia.id = id;
+            videoMedia.tabIndex = "0";
             divMedia.appendChild(videoMedia);
         } else {
             // Create image
             const imgMedia = document.createElement("img");
             imgMedia.src = mediaUrl;
             imgMedia.alt = title;
+            imgMedia.title = title;
             imgMedia.classList.add("viewIn");
             imgMedia.id = id;
+            imgMedia.tabIndex = "0";
             divMedia.appendChild(imgMedia);
         }
 
