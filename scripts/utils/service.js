2e32cd26fb7083d98bcc0364ef7977f6959a0864
Sayaka Vincent <sfv2010@hotmail.fr>
2023-03-20 23:03:44 +0100

    rename constant

--- scripts/utils/service.js
+++ scripts/utils/service.js
@@ -2,8 +2,8 @@
 export async function getPhotographers() {
     try {
         const resPhotographers = await fetch("data/photographers.json");
-        const jsonPhotographers = await resPhotographers.json();
-        return jsonPhotographers;
+        const jsPhotographers = await resPhotographers.json();
+        return jsPhotographers;
     } catch (error) {
         console.log(error);
         document.querySelector(".photographer_section").textContent =
